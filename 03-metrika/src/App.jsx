7a0794958b4fa573d1408966766c5ec8a82ae3e4
Marianita2007 <gomezcorreamariana2@gmail.com>
2025-11-20 13:22:57 -0500

    Metrika

--- 03-metrika/src/App.jsx
+++ 03-metrika/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import { SectionCard } from '../components/SectionCard'
 
-const App = () => {
+
+export const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div>
+        <h1 className='title-metrika'>Metrika</h1>
+        <img src="../assets/icons-redes.png" alt="" />
+      </div>
+      <SectionCard />
+
+    </>
   )
 }
-
-export default App
-
